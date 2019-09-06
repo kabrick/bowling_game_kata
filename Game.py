0f4f2a140e1a63a12a78546de8453fc27f5732e8
kabrick <kabuyedouglas53@gmail.com>
2019-09-06 08:31:21 +0300

    implement spare logic

--- Game.py
+++ Game.py
@@ -8,8 +8,10 @@ class Game:
         self.pins = 10
         self.pins_knocked_down = 0
         self.pins_knocked_down_in_frame = 0
-        self.strikes = True
-        self.spares = True
+        self.strikes = False
+        self.strikes_bonus = 0
+        self.spares = False
+        self.spares_bonus = 0
 
     def roll(self, pins):
         if pins < 0:
@@ -21,14 +23,22 @@ class Game:
         if (self.pins_knocked_down_in_frame + pins) > 10:
             raise ValueError("You can not enter more than 10 pins per frame")
 
+        if self.spares:
+            self.spares_bonus += pins
+            self.spares = False
+
         if self.current_roll == 1 and pins == 10:
             self.current_roll = 2
 
         self.pins_knocked_down_in_frame += pins
         self.pins_knocked_down += pins
 
+        if self.pins_knocked_down_in_frame == 10:
+            self.spares = True
+
         if self.current_roll == 2:
             self.current_roll = 1
             self.current_frames += 1
+            self.pins_knocked_down_in_frame = 0
         else:
             self.current_roll += 1
